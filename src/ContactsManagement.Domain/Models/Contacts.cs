866c423c06a0e749dd13e659bc62348569b93dd4
Danielle Baer <danielle.baer@hotmail.com>
2024-05-19 09:53:54 -0300

    feat: add contracts and migrations

--- src/ContactsManagement.Domain/Models/Contacts.cs
+++ src/ContactsManagement.Domain/Models/Contacts.cs
@@ -1,7 +1,21 @@
 ﻿namespace ContactsManagement.Domain.Models;
 
-public class Contacts
+public record Contacts
 {
-    public int Id { get; set; }
+    public Contacts()
+    {
+        NavigationId = Guid.NewGuid();
+    }
+
+    public Guid NavigationId { get; set; }
+
     public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Ddd { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public Region? Region { get; set; }
 }
---
+++ src/ContactsManagement.Domain/Models/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsManagement.Domain.Models;
+
+public record Region
+{
+    public Guid NavigationId { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Ddd { get; set; }
+}
